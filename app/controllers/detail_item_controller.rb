fb150d5b244facbe7e40dd7e7a05f94567658557
Minkyung <mnk918p@gmail.com>
2018-08-16 21:11:03 +0900

    최근 게시물 위로

--- app/controllers/detail_item_controller.rb
+++ app/controllers/detail_item_controller.rb
@@ -2,6 +2,6 @@ class DetailItemController < ApplicationController
   def index
     get_bread = params[:bread_name]
     @bread = Bread.find_by(bread_name: get_bread)
-    @review = @bread.reviews
+    @review = @bread.reviews.order("created_at DESC")
   end
 end
