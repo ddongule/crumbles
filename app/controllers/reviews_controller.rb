89560b57dc855fcf17db0423edae73b7cca66a4d
LDongGyu <ehdrbchd@naver.com>
2018-08-21 19:21:40 +0900

    빵으로 찾기 경로 오타 수정

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -36,6 +36,8 @@ class ReviewsController < ApplicationController
         review = Review.find(params[:review_id])
         review.content = params[:input_content]
         review.star_point = params[:input_starPoint]
+        review.image = review_params.delete(:image)
+        review.update_attributes(review_params)
         review.image = params[:input_img]
 
         review.save
